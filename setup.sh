0d417884b300a5f7589976718ae4683ae51ff16f
Dustin Mowcomber <dustin.mowcomber@sendgrid.com>
2017-08-15 11:38:39 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-unlink ~/.zshrc || true
-unlink ~/.bashrc || true
-unlink ~/.oh-my-zsh || true
-unlink ~/.gitconfig || true
-unlink /usr/local/bin/subl || true
+unlink ~/.zshrc
+unlink ~/.vimrc
+unlink ~/.bashrc
+unlink ~/.oh-my-zsh
+unlink ~/.gitconfig
+unlink /usr/local/bin/subl
 
 ln -s ~/env/.bashrc ~/.bashrc
 ln -s ~/env/.zshrc ~/.zshrc
+ln -s ~/env/.vimrc ~/.vimrc
 ln -s ~/env/.oh-my-zsh ~/.oh-my-zsh
 ln -s ~/env/.gitconfig ~/.gitconfig
 ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
