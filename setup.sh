1af0f1f1cb0c03c109543db0b56c9d698b126893
Dustin Mowcomber <dustin.mowcomber@sendgrid.com>
2017-08-15 20:53:40 -0700

    update setup.sh to know where env is cloned

--- setup.sh
+++ setup.sh
@@ -10,10 +10,12 @@ unlink ~/.oh-my-zsh
 unlink ~/.gitconfig
 unlink /usr/local/bin/subl
 
-ln -s ~/env/.bashrc ~/.bashrc
-ln -s ~/env/.zshrc ~/.zshrc
-ln -s ~/env/.vimrc ~/.vimrc
-ln -s ~/env/.vim ~/.vim
-ln -s ~/env/.oh-my-zsh ~/.oh-my-zsh
-ln -s ~/env/.gitconfig ~/.gitconfig
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -s $DIR/bashrc ~/.bashrc
+ln -s $DIR/.zshrc ~/.zshrc
+ln -s $DIR/.vimrc ~/.vimrc
+ln -s $DIR/.vim ~/.vim
+ln -s $DIR/.oh-my-zsh ~/.oh-my-zsh
+ln -s $DIR/.gitconfig ~/.gitconfig
 ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
